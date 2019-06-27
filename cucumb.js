5a1ef6b7a028d74c01200947986b07bc4359ad02
iskomt <iskomt@yahoo.com>
2019-06-27 16:07:07 -0500

    Hi

--- cucumb.js
+++ cucumb.js
@@ -3,22 +3,23 @@
 
 // Dependencies
 //
-const express = require('express');
-const httpShutdown = require('http-shutdown');
-const path = require('path');
+const express = require("express");
+const httpShutdown = require("http-shutdown");
+const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Listen on the port
-const server = httpShutdown(app.listen(3000, () => {
-	console.log(`Web is listening on port 3000`);
-}));
+const server = httpShutdown(
+  app.listen(3000, () => {
+    console.log(`Web is listening on port 3000`);
+  })
+);
 
 server.host = `http://localhost:3000`;
 
