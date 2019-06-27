c427a087c4a66e76439a0b18b465bd4f9ed0bf5c
iskomtdev <iskomt@yahoo.com>
2019-06-27 16:32:16 -0500

    Remove this title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-import './css/style.css'
-import Navbar from './components/navbar'
-import Home from './components/home'
-import ActorsContainer from './components/actorsContainer'
-import Actress from './components/actress'
-import Films from './components/films'
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./css/style.css";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import ActorsContainer from "./components/actorsContainer";
+import Actress from "./components/actress";
+import Films from "./components/films";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Navbar title="Oscars 2019"/>
-          
-          <Route exact path="/" render={() => <Home title="Oscar Winners" />}/>
-          <Route path="/actors" render={() => <ActorsContainer title="Best Actors" />}/>
-          <Route path="/actress" render={() => <Actress title="Best Actresses" />}/>
-          <Route path="/films" render={() => <Films title="Best films" />}/>
+          <Route exact path="/" render={() => <Home title="Oscar Winners" />} />
+          <Route
+            path="/actors"
+            render={() => <ActorsContainer title="Best Actors" />}
+          />
+          <Route
+            path="/actress"
+            render={() => <Actress title="Best Actresses" />}
+          />
+          <Route path="/films" render={() => <Films title="Best films" />} />
           {/*</Route><Home title="Oscar Winners" />*/}
         </div>
       </BrowserRouter>
